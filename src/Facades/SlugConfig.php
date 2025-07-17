2780dde2de0ce45c238c48dab5e46db912314fd2
Niels Hamelink <niels@gyvex.com>
2025-07-18 00:10:34 +0200

    Fixed autocomplete on Facade & improved README

--- src/Facades/SlugConfig.php
+++ src/Facades/SlugConfig.php
@@ -4,6 +4,11 @@ namespace VeiligLanceren\LaravelWebshopProduct\Facades;
 
 use Illuminate\Support\Facades\Facade;
 
+/**
+ * @method static \Spatie\Sluggable\SlugOptions get(string $key)
+ *
+ * @see \VeiligLanceren\LaravelWebshopProduct\Services\Seo\SlugConfigService
+ */
 class SlugConfig extends Facade
 {
     protected static function getFacadeAccessor(): string
